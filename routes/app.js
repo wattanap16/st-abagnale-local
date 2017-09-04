6aa2844ddcadbef9c4591041e35fab0b1e213fed
bank wattanap <test@test.com>
2017-09-04 02:54:18 -0700

    after Section 4 part 43

--- routes/app.js
+++ routes/app.js
@@ -1,26 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var User = require('../models/user')
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc){
-        if (err){
-            return res.send('Error!');
-        }
-        res.render('node', {email: doc.email});
-    });
+    res.render('index');
 });
 
-router.post('/', function(req, res, next){
-    var email =req.body.email;
-    var user = new User({
-        firstName: 'Thanathamma',
-        lastName: 'Tayapitak',
-        password: 'super-secret',
-        email: email
-    });
-    user.save();
-    res.redirect('/');
-});
 module.exports = router;
 
