49c4a9fb28a0320122958e26f33e11a487616c42
bank wattanap <test@test.com>
2017-09-07 22:47:05 -0700

    after Section 5 part 68 Deleting Messages

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { MessageService } from "./message.service";
 
 import { Message } from "./message.model";
@@ -8,18 +8,40 @@ import { NgForm } from "@angular/forms";
      selector: "app-message-input",
      templateUrl: './message-input.component.html'
 })
-export class MessageInputComponent {
+export class MessageInputComponent implements OnInit {
+    message: Message;
+
     constructor(private messageService: MessageService) {}
 
     onSubmit(form: NgForm) {
-        
-         const message = new Message(form.value.content, 'fairy');
+        if (this.message) {
+            this.message.content = form.value.content;
+            this.messageService.updateMessage(this.message)
+                .subscribe(
+                    result => console.log(result)
+                );
+            this.message = null;
+        } else {
+           const message = new Message(form.value.content, 'fairy');
          this.messageService.addMessage(message)
             .subscribe(
                 data => console.log(data),
                 error => console.log(error)
-            );
+            ); 
+        }
+         
          form.resetForm();
+     }
+
+     onClear(form: NgForm) {
+         this.message = null;
+         form.resetForm();
+     }
+
+     ngOnInit() {
+         this.messageService.messageIsEdit.subscribe(
+             (message: Message) => this.message = message
+         );
      }
     
 }
--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -8,8 +8,7 @@ import { MessageService } from "./message.service";
         <div class="col-md-8 col-md-offset-2">
            <app-message 
                [message]="message" 
-               (editClicked)="message.content = $event"
-               *ngFor="let message of messages"></app-message>
+                *ngFor="let message of messages"></app-message>
         </div>
     `
 })
@@ -19,6 +18,11 @@ export class MessageListComponent implements OnInit {
     constructor(private messageService: MessageService) {}
 
     ngOnInit() {
-        this.messages = this.messageService.getMessage();
+        this.messageService.getMessage()
+        .subscribe(
+            (messages: Message[]) => {
+                  this.messages = messages;
+            }
+        );
     }
 }
