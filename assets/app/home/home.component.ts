9df74a057d6be5918f7dd7b47fc7e6a654f5c95b
bank wattanap <test@test.com>
2017-10-27 03:29:21 -0700

    add home paralax

--- assets/app/home/home.component.ts
+++ assets/app/home/home.component.ts
@@ -7,8 +7,57 @@ import { Component, OnInit} from "@angular/core";
     selector: 'app-home',
     templateUrl: './home.component.html',
     styles: [`
-        h1 {
+        .course-box {
+            position: relative;
+            height: 600px;
+            background-image: url('/images/homeparalax/g1.jpg');
+            background-repeat: no-repeat;
+            background-size: 100% 600px;
+            background-position: top center;
+            background-attachment: fixed;
+            overflow: hidden;
+        }
+
+        .golfer {
+            width: 960px;
+            height: 280px;
+            background-image: url('/images/homeparalax/gf1.png');
+            background-image-color: transparent;
+            background-repeat: no-repeat;
+            background-color: transparent;
+            background-position: center;
+            position: absolute;
+            margin-top: 240px;
+        }
+
+        .tree {
+            width: 100%;
+            height: 480px;
+           
+            background-image-color: transparent;
+            background-repeat: no-repeat;
+            background-color: transparent;
+            background-position:  top left;
+            position: absolute;
+            margin-top:-300px;
+
+        }
+        
+        .fan {
+            width: 1755px;
+            height: 600px;
+            background-image: url('/images/homeparalax/fan1.png');
+            background-image-color: transparent;
+            background-repeat: no-repeat;
+            background-color: transparent;
+            background-position: bottom ;
+            position: absolute;
+            margin-top:350px;
             
+        }
+
+        h1 {
+            color: transparent;
             font-style:  italic;
             font-size: 70px;
             width: 100%;
@@ -30,11 +79,14 @@ import { Component, OnInit} from "@angular/core";
     `]
 })
 export class HomeComponent implements OnInit{
-    fullImagePath: string;
+    fullImagePath: [string];
     
       constructor() {
        
-        this.fullImagePath = ('/images/g1.jpg');
+        this.fullImagePath = [('/images/homeparalax/fan1.png'),
+                              ('/images/homeparalax/g1.jpg'),
+                              ('/images/homeparalax/golfer1.jpg'),
+                              ('/images/homeparalax/tree2.jpg')];
       }
     
       ngOnInit() {
